55c4ec8cf400f26f5ed07e1a9492494ca87bad35
Gizachew Bayness <eleccrazy24@gmail.com>
2022-06-27 05:43:57 -0700

    Bug Fixed

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -16,10 +16,10 @@ int check_cycle(listint_t *list)
 	if (list == NULL)
 		return (0);
 
-	slow = list->next;
-	fast = list->next->next;
+	slow = list;
+	fast = list->next;
 
-	while (fast)
+	while (slow && fast && fast->next)
 	{
 		if (slow ==  fast)
 			return (1);
