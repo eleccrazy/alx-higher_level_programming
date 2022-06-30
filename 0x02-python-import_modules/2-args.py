eba48ac1ecdc74f790667e3ed2706335b13a790e
Gizachew Bayness <eleccrazy24@gmail.com>
2022-06-30 06:34:23 -0700

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+    i = 1
+    if len(argv) < 2:
+        print("1 argument.")
+    elif len(argv) == 2:
+        print("{:d} argument:".format(len(argv) - 1))
+        print("{:d}: {:s}".format((len(argv) - 1), argv[1]))
+    else:
+        print("{} arguments".format(len(argv) - 1))
+        while (i < len(argv)):
+            print("{:d}: {:s}".format(i, argv[i]))
+            i += 1
+        exit()
