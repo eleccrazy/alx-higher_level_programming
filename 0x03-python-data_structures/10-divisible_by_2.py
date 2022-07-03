6f7ac473487197ebab0eac12f3cdb3e41343033c
Gizachew Bayness <eleccrazy24@gmail.com>
2022-07-03 03:47:04 -0700

    task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def divisible_by_2(my_list=[]):
+    new_list = []
+    for number in my_list:
+        if number % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+    return new_list
