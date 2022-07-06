349cb3713d70eb4fcbcfe4c56534cdff8bfd03ab
Gizachew Bayness <eleccrazy24@gmail.com>
2022-07-06 10:09:43 -0700

    task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def update_dictionary(a_dictionary, key, value):
+    if not key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        for keyy in a_dictionary:
+            if keyy == key:
+                a_dictionary[key] = value
+    return a_dictionary
