7062218bd8e6721445405b31c72b6256e64ff86a
Gizachew Bayness <eleccrazy24@gmail.com>
2022-07-06 10:01:01 -0700

    pycodestyle fixed

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 
 
 def print_sorted_dictionary(a_dictionary):
-     [print("{}: {}".format(i, a_dictionary[i])) for i in sorted(a_dictionary)]
+    [print("{}: {}".format(i, a_dictionary[i])) for i in sorted(a_dictionary)]
