ea9b165aadb135d6ba50b4f0165a234f27dd3188
Gizachew Bayness <eleccrazy24@gmail.com>
2022-07-06 11:13:05 -0700

    Bug Fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,12 +2,13 @@
 
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-    large = a_dictionary[list(a_dictionary.keys())[0]]
-    big_key = list(a_dictionary.keys())[0]
-    for key, value in a_dictionary.items():
-        if value > large:
-            large = value
-            big_key = key
-    return big_key
+
+    ret = list(a_dictionary.keys())[0]
+    big = a_dictionary[ret]
+    for k, v in a_dictionary.items():
+        if v > big:
+            big = v
+            ret = k
+    return (ret)
