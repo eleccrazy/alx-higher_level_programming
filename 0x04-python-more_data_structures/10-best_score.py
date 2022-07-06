57db2c26d956f3d41f9d299c6abe2477e5e62a59
Gizachew Bayness <eleccrazy24@gmail.com>
2022-07-06 10:58:27 -0700

    task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    keys = list(a_dictionary.keys())
+    index = 0
+    values = list(a_dictionary.values())
+    large = values[0]
+    for num in values:
+        if num > large:
+            large = num
+    for la in values:
+        if la == large:
+            break
+        index += 1
+    return keys[index]
