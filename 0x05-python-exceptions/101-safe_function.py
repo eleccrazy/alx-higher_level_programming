17ef7e7fa8b9edc75ad9832ba7dd47ec3e7ef510
Gizachew Bayness <eleccrazy24@gmail.com>
2022-07-18 09:06:20 -0700

    task 8

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        ret_val = fct(*args)
+    except:
+        msg = sys.exc_info()
+        print("Exception: {}".format(msg[1]), file=sys.stderr)
+        ret_val = None
+    finally:
+        return ret_val
