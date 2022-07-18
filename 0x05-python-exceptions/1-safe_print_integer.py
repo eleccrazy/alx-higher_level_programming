1c451426c14994e4603e0c8ac16b82b422036732
Gizachew Bayness <eleccrazy24@gmail.com>
2022-07-18 06:00:59 -0700

    Bug Hunted

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,9 @@
 
 
 def safe_print_integer(value):
+    status = True
     try:
         print("{:d}".format(value))
     except ValueError:
-        return False
-    return True
+        status = False
+    return status
