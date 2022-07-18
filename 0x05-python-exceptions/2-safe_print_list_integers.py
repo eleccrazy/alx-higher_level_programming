13345f8eaae187a4fefecb41085af3422276b41e
Gizachew Bayness <eleccrazy24@gmail.com>
2022-07-18 06:19:33 -0700

    task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for index in range(x):
+        try:
+            print("{:d}".format(my_list[index]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return count
