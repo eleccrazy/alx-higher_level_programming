d11bc92730128158a3572abd61328b95a5736681
Gizachew Bayness <eleccrazy24@gmail.com>
2022-07-19 06:52:28 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+File: 1-square.py
+Desc: This module contains a single class defination Square.
+Author: Gizachew Bayness (Elec Crazy)
+Date Created: Jul 19 2022
+"""
+
+
+class Square():
+    """
+    This class contains a defination of a single private object attribute
+    called size.
+    """
+    def __init__(self, size):
+        self.__size = size
