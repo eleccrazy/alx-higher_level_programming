75db167145cdaab0ea5e60e687b79c217ab9c702
Gizachew Bayness <eleccrazy24@gmail.com>
2022-07-19 06:41:58 -0700

    Documentation Added

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 
+"""
+File: 0-square.py
+Desc: This module contains a single empty class defination.
+Author: Gizachew Bayness (Elec Crazy)
+Date Created: Jul 19 2022
+"""
+
 
 class Square():
     """
     This is an empty class created for showing how to create
-    a class.
+    a class and it does nothing.
     """
     pass
