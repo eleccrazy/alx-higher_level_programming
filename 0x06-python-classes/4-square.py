fc6083332f9d884faf13660258ff6a4fc7653842
Gizachew Bayness <eleccrazy24@gmail.com>
2022-07-19 10:11:35 -0700

    Fulfilling DRY of coding zen

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -14,12 +14,7 @@ class Square():
     definations.
     """
     def __init__(self, size=0):
-        if isinstance(size, int):
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
-        else:
-            raise TypeError("size must be an integer")
+        self.size = size
 
     @property
     def size(self):
