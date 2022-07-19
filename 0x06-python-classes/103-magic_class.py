2b4ba3b125bd5fcbbf472ee8df0081bf7b92a20f
Gizachew Bayness <eleccrazy24@gmail.com>
2022-07-19 13:35:04 -0700

    Docstring Added

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
+
+"""
+File: 103-magic_class.py
+Desc: This file contains the python code of reveresed version of
+python byte code.
+Author: Gizachew Bayness (Elec Crazy)
+Date Created: Jul 19 2022
+
+Reverse Engineering is really awesome!
+"""
 import math
 
 
 class MagicClass():
+    """
+    MagicClass representation of python byte code given.
+    """
     def __init__(self, radius=0):
         self.__radius = 0
 
@@ -12,7 +25,13 @@ class MagicClass():
             self.__radius = radius
 
     def area(self):
+        """
+        This method computes the area of the given circle.
+        """
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
+        """
+        This method computes the circumstance of the given circle.
+        """
         return 2 * math.pi * self.__radius
