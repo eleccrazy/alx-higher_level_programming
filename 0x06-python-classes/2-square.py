556cf30d685ada68172621df7a4efaa620645c2e
Gizachew Bayness <eleccrazy24@gmail.com>
2022-07-19 07:18:05 -0700

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+File: 2-square.py
+Desc: Thi module contains a single class defination called Size.
+Author: Gizachew Bayness (Elec Crazy)
+Date Created: Jul 19 2022
+"""
+
+
+class Square():
+    """
+    This class contains a defination of a single private object attribute
+    called size, and checks the attribute if it is integer and positive.
+    """
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
