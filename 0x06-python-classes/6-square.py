b362c416a99f9d3d871cbddaa506eaea848e3cf1
Gizachew Bayness <eleccrazy24@gmail.com>
2022-07-19 09:12:35 -0700

    task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+
+"""
+File: 6-square.py
+Desc: This module contains a single class defination called Size.
+Author: Gizachew Bayness (Elec Crazy)
+Date Created: Jul 19 2022
+"""
+
+
+class Square():
+    """
+    This square class contains some attribute definations, and method
+    definations.
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """
+        This method retrives the value of attribute size.
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+        This method sets the value of the attribute size.
+        """
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    @property
+    def position(self):
+        """
+        This method retrives the value of the attribute position.
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """
+        This method sets the value of the attribute position.
+        """
+        if isinstance(vaue, tuple) and len(value) == 2 and any(value) < 0:
+            self.__position = value
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        """
+        This method computes and returns the square based on the size.
+        """
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """
+        This method prints the square with the character '#' based on the size.
+        """
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                if not (self.__position[1] > 1):
+                    for k in range(self.__position[0]):
+                        print(" ", end="")
+                for j in range(self.__size):
+                    print('#', end="")
+                print("")
