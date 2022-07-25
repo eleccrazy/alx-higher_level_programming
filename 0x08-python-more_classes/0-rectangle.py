8275aa7e750390a8cc4d29b991285608af42649b
Gizachew Bayness <eleccrazy24@gmail.com>
2022-07-25 11:33:08 -0700

    task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+File: 0-rectangle.py
+Desc: This file contains a simple class defination called Rectangle
+Author: Gizachew Bayness (Elec Crazy)
+Date Created: Jul 20, 2022
+"""
+
+
+class Rectangle():
+    """
+    This is an empty class defination called Rectangle.
+    """
+    pass
