4c51199aac759e2ac1421e5df6f2427e327f2e8e
Gizachew Bayness <eleccrazy24@gmail.com>
2022-07-26 08:55:49 -0700

    task 30

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+File: 101-locked_class.py
+Desc: This module contains a single class defination.
+Author: Gizachew Bayness (Elec Crazy)
+Date Created: Jul 26, 2022
+"""
+
+
+class LockedClass():
+    """
+    This class prevents the user from dynamically creating
+    new instance attributes, except if the new instance 
+    attribute is called first_name
+    """
+    __slots__ = ["first_name"]
