2571295b78d4bbee3f367f8733849159487d9aad
Gizachew Bayness <eleccrazy24@gmail.com>
2022-08-01 12:47:56 -0700

    task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+File: 11-square.py
+Desc: This modlue contains a single class defination.
+Author: Gizachew Bayness (Elec Crazy)
+Date Created: Aug 1 2022
+"""
+R = __import__('9-rectangle').Rectangle
+
+
+class Square(R):
+    """
+    This class inheretes from Rectangle class.
+    """
+    def __init__(self, size):
+        """
+        This function initializes the size attribute.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
