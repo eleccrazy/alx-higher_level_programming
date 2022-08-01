652af67a033961644123e2a8815c4af9c5c1d28b
Gizachew Bayness <eleccrazy24@gmail.com>
2022-08-01 11:12:03 -0700

    task 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+File: 5-base_geometry.py
+Desc: This module defines an empty class.
+Author: Gizachew Bayness (Elec Crazy).
+Date Created: Aug 1 2022
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class defination.
+    """
