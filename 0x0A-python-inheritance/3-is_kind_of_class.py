d7cab2f4fe0184f59c5f879498854a5b3f3e633a
Gizachew Bayness <eleccrazy24@gmail.com>
2022-08-01 10:59:45 -0700

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/pytho3
+
+"""
+File: 3-is_kind_of_class.py
+Desc: This file contains a single function defination
+Author: Gizachew Bayness (Elec Crazy)
+Date Created: Aug 1 2022
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    This function returns True if the object is an instance of,
+    or if the object is an instance of a class that inherited
+    from, the specified class ; otherwise False.
+    """
+
+    if isinstance(obj, a_class):
+        return True
+    return False
