28196a94018f721370ec3a8fd6809b9e49b63055
Gizachew Bayness <eleccrazy24@gmail.com>
2022-08-01 11:16:27 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+File: 6-base_geometry.py
+Desc: This module defines an empty class.
+Author: Gizachew Bayness (Elec Crazy).
+Date Created: Aug 1 2022
+"""
+
+
+class BaseGeometry:
+    """
+    A simple class defination.
+    """
+    def area(self):
+        """
+        A function that raises an Exception.
+        """
+        raise Exception("area() is not implemented")
