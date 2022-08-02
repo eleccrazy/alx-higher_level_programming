4506416817d8061dac157e0b5d5bed5d690eaf22
Gizachew Bayness <eleccrazy24@gmail.com>
2022-08-02 00:41:34 -0700

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+File: 0-read_file.py
+Desc: This module deals with working with files.
+Author: Gizachew Bayness (Elec Crazy)
+Date Created: Aug 2 2022
+"""
+
+
+def read_file(filename=""):
+    """
+    This function reads a text file (UTF8) and prints it to stdout.
+    """
+
+    with open(filename, encoding="utf8") as my_file:
+        for text in my_file:
+            print(text, end="")
