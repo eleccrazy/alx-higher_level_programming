f98811d465f5e2432850e280164b4929d86354b0
Gizachew Bayness <eleccrazy24@gmail.com>
2022-08-02 05:22:59 -0700

    Requirement fixed

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -16,9 +16,9 @@ def append_after(filename="", search_string="", new_string=""):
     with open(filename, encoding="utf8") as my_file:
         line_list = my_file.readlines()
         new_content = ""
-        for l in line_list:
-            new_content += l
-            if search_string in l:
+        for line in line_list:
+            new_content += line
+            if search_string in line:
                 new_content += new_string
     with open(filename, "w", encoding="utf8") as f:
         f.write(new_content)
