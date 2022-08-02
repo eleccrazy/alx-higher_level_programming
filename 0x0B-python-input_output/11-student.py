9f1d3459a5e5ea6231c665a82d7e6aba524d5f3a
Gizachew Bayness <eleccrazy24@gmail.com>
2022-08-02 04:23:36 -0700

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+File: 11-student.py
+Desc: This module deals with classes and json
+Author: Gizachew Bayness (Elec Crazy)
+Date Created: Aug 2 2022
+"""
+
+
+class Student():
+    """
+    A simple stundent class.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        This function initializes the object parameters.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        This function retrieves a dictionary representation
+        of a Student instance
+        """
+        if type(attrs) == list and all(type(i) == str for i in attrs):
+            return ({key: getattr(self, key)
+                    for key in attrs if hasattr(self, key)})
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        This function replaces all attributes of the Student instance
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
