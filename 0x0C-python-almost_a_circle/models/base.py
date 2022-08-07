f0aa8bc57926cf3e6310cf577883ca88ba99e4a6
Gizachew Bayness <eleccrazy24@gmail.com>
2022-08-07 13:09:15 -0700

    Update base module

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,6 +6,7 @@ Desc: This module contains a single class calld Base.
 Author: Gizachew Bayness (Elec Crazy)
 Date Created: Aug 6 2022
 """
+from json import dumps as ds
 
 
 class Base:
@@ -23,3 +24,26 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        This method  returns the JSON string representation
+        of list_dictionaries
+        """
+        if list_dictionaries is None:
+            return "[]"
+        return ds(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        This method writes the JSON string representation of
+        list_objs to a file
+        """
+        with open(cls.__name__+".json", "w", encoding="utf8") as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                ls = [obj.to_dictionary() for obj in list_objs]
+                f.write(Base.to_json_string(ls))
