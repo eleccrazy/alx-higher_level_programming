da8c25fceac4d1d83ed23045fd2a011e04cc067c
Gizachew Bayness <eleccrazy24@gmail.com>
2022-08-07 04:30:53 -0700

    Area added

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,3 +82,7 @@ class Rectangle(B):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """ Computes and returns the area of the rectangle """
+        return self.__height * self.__width
