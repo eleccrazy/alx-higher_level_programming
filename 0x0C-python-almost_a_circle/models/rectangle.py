f718fe149e9419c02a1d45ae54419ce5314e6f8a
Gizachew Bayness <eleccrazy24@gmail.com>
2022-08-07 04:58:46 -0700

    Update display method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -92,7 +92,10 @@ class Rectangle(B):
         prints in stdout the Rectangle instance with the
         character #
         """
+        for y in range(self.__y):
+            print()
         for h in range(self.__height):
+            [print(' ', end='') for x in range(self.__x)]
             [print('#', end='') for w in range(self.__width)]
             print()
 
