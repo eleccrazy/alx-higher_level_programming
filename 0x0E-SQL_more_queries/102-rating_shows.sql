5041988f1257d49560f1dfc506fc8b3bb4d9d0e0
Gizachew Bayness <eleccrazy24@gmail.com>
2022-09-07 15:25:55 -0400

    task 19

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,4 @@
+-- This script lists all shows from hbtn_0d_tvshows_rate by their rating.
+SELECT title, SUM(rate) AS rating FROM tv_shows AS t
+INNER JOIN tv_show_ratings AS r ON t.id = r.show_id
+GROUP BY title ORDER BY rating DESC;
