7e69cec6f4dde30f39ac313bb97bd157f10668fb
Gizachew Bayness <eleccrazy24@gmail.com>
2022-09-07 14:41:05 -0400

    fix

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,11 +1,9 @@
 -- This script creates the database hbtn_0d_2 and the user user_0d_2.
 CREATE DATABASE
     IF NOT EXISTS `hbtn_0d_2`;
-CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'
-    IDENTIFIED BY 'user_0d_2_pwd';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2@localhost;
+SET PASSWORD FOR user_0d_2@localhost = 'user_0d_2_pwd';
 GRANT SELECT
-   ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
-FLUSH PRIVILEGES;
+	ON hbtn_0d_2.*
+	TO user_0d_2@localhost;
