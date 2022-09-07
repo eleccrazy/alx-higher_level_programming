b55d02aeb50e0ce4e6a2d4a184ec32e34414f40e
Gizachew Bayness <eleccrazy24@gmail.com>
2022-09-07 14:38:18 -0400

    fix

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,7 @@
 -- This script creates a new user with all privilages
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS user_0d_1@localhost;
+SET PASSWORD FOR user_0d_1@localhost = 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+	ON *.*
+	TO user_0d_1@localhost;
 FLUSH PRIVILEGES;
