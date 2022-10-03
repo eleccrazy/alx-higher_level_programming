a08e815bde2a6af1aa8abe7a7096d8a0be059606
Gizachew Bayness <eleccrazy24@gmail.com>
2022-10-03 09:22:50 +0300

    Semistandard fixed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,12 +2,10 @@
 
 const numberOfArguments = process.argv.length;
 
-if (numberOfArguments === 2){
-	console.log("No argument");
-}
-else if (numberOfArguments === 3){
-	console.log("Argument found");
-}
-else {
-	console.log("Arguments found");
+if (numberOfArguments === 2) {
+  console.log('No argument');
+} else if (numberOfArguments === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
