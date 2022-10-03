ac43d71ed44511775365fbc87bf8e932ad064c07
Gizachew Bayness <eleccrazy24@gmail.com>
2022-10-03 09:19:58 +0300

    task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const numberOfArguments = process.argv.length;
+
+if (numberOfArguments === 2){
+	console.log("No argument");
+}
+else if (numberOfArguments === 3){
+	console.log("Argument found");
+}
+else {
+	console.log("Arguments found");
+}
