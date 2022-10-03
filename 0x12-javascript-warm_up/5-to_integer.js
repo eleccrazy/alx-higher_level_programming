2d00734c9a68d60fbd4cdcf3b342c118e0bff391
Gizachew Bayness <eleccrazy24@gmail.com>
2022-10-03 09:51:30 +0300

    Fixed logic on floating number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
 const num = process.argv[2];
-!Number(num) ? console.log('Not a number') : console.log('My number: ' + num);
+!parseInt(num) ? console.log('Not a number') : console.log('My number: ' + parseInt(num));
