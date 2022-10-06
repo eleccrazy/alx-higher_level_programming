0383c56f56c566ce7a085b6b874b5f4b9a4c026b
Gizachew Bayness <eleccrazy24@gmail.com>
2022-10-06 22:34:01 +0300

    execution when importing

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,18 +2,18 @@
 
 from sys import argv
 import MySQLdb
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
 
-db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
-        )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    states_info = cur.fetchall()
 
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-states_info = cur.fetchall()
-
-for state in states_info:
-    print(state)
+    for state in states_info:
+        print(state)
