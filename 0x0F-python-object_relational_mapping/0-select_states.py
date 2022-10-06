2dae90989d8a29f3d585a27b4f9ca9d263d8651c
Gizachew Bayness <eleccrazy24@gmail.com>
2022-10-06 22:14:42 +0300

    taks 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+from sys import argv
+import MySQLdb
+
+db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+        )
+
+cur = db.cursor()
+cur.execute("SELECT * FROM states")
+states_info = cur.fetchall()
+
+for state in states_info:
+    print(state)
