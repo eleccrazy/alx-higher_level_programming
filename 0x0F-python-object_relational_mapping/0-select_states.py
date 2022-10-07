6d873719831170de56053c350bbef28cfb4f30f2
Gizachew Bayness <eleccrazy24@gmail.com>
2022-10-07 09:39:53 +0300

    Docs added

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+file: 0-select_states.py
+Desc: This module contains a script to  lists all
+states from the database hbtn_0e_0_usa
+
+Author: Gizachew Bayness (Elec Crazy).
+
+Date Created: oct 7, 2022
+"""
 
 from sys import argv
 import MySQLdb
