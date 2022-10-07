93b5ea2cd95af00af12e891e8bd0495e87f15da2
Gizachew Bayness <eleccrazy24@gmail.com>
2022-10-07 15:03:11 +0300

    task 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+File: 7-model_state_fetch_all.sql
+Desc: This module contains a python script that lists all
+State objects from the database hbtn_0e_6_usa
+
+Author: Gizachew Bayness (Elec Crazy).
+Date Created: Oct 7 2022
+"""
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State)
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
