f50f92a7a3a57ece85b102f03b7dec5d545bb550
Gizachew Bayness <eleccrazy24@gmail.com>
2022-10-07 09:43:18 +0300

    Docs added

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 
+"""
+File: 1-filter_states.py
+Desc: This module contains a script that  lists all states
+with a name starting with N (upper N) from the database hbtn_0e_0_usa
+
+Author: Gizachew Bayness (Elec Crazy)
+Date Created: Oct 7, 2022
+"""
+
 from sys import argv
 import MySQLdb
 
