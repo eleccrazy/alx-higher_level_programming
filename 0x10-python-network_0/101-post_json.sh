c55e3e15e279620905576befad660916b0e3175a
Gizachew Bayness <eleccrazy24@gmail.com>
2022-10-20 23:00:13 +0300

    task 8

---
+++ 0x10-python-network_0/101-post_json.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This bash script sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
+curl -X POST "$1" -H 'Content-Type: application/json' -d @"$2"
