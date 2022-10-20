b9e9e6bd40c980b7d3e624f184c7a66798be18e9
Gizachew Bayness <eleccrazy24@gmail.com>
2022-10-20 22:15:30 +0300

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""This module contains a function that finds a peak in a list
+of unsorted integers."""
+
+
+def find_peak(list_of_integers):
+    if list_of_integers == []:
+        return None
+
+    if len(list_of_integers) < 3:
+        return max(list_of_integers)
+
+    max_index = int(len(list_of_integers) / 2)
+    peack_number = list_of_integers[max_index]
+
+    if (peack_number > list_of_integers[max_index - 1] and
+            peack_number > list_of_integers[max_index + 1]):
+        return peack_number
+
+    elif peack_number <= list_of_integers[max_index + 1]:
+        return find_peak(list_of_integers[max_index + 1:])
+
+    else:
+        return find_peak(list_of_integers[:max_index])
