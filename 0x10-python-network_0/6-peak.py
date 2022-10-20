ff673fa2c4871ba51db9be808a98aa81ed491f2f
Gizachew Bayness <eleccrazy24@gmail.com>
2022-10-20 22:33:31 +0300

    Efficiency fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -20,8 +20,8 @@ def find_peak(list_of_integers):
             peack_number > list_of_integers[max_index + 1]):
         return peack_number
 
-    elif peack_number < list_of_integers[max_index - 1]:
-        return find_peak(list_of_integers[:max_index])
+    elif peack_number <= list_of_integers[max_index + 1]:
+        return find_peak(list_of_integers[max_index + 1:])
 
     else:
-        return find_peak(list_of_integers[max_index + 1:])
+        return find_peak(list_of_integers[:max_index])
