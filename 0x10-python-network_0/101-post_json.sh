ef23b78ba9d0c70573ef8ccac35edd3ec8778dfe
Gizachew Bayness <eleccrazy24@gmail.com>
2022-10-20 23:10:04 +0300

    Fix with silent option

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This bash script sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -X POST "$1" -H 'Content-Type: application/json' -d @"$2"
+curl -s -X POST "$1" -H 'Content-Type: application/json' -d @"$2"
