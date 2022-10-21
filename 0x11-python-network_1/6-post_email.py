ec633bd4e304ae2d04068be019138afb77d61794
Gizachew Bayness <eleccrazy24@gmail.com>
2022-10-21 22:50:41 +0300

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This python module contains a script that takes in a URL and an
+email address, sends a POST request to the passed URL with the
+email as a parameter, and finally displays the body of the
+response. """
+from sys import argv
+from requests import post
+
+
+if __name__ == "__main__":
+    payload = {'email': argv[2]}
+    result = post(argv[1], data=payload)
+    result.text
