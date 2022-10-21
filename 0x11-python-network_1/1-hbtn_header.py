6f35c6a136744470a1b8f193b8ed3b1ede811e29
Gizachew Bayness <eleccrazy24@gmail.com>
2022-10-21 21:25:45 +0300

    task 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This pyhton module consists a script that takes in a URL,
+sends a request to the URL and displays the value of the
+X-Request-Id variable found in the header of the response."""
+from sys import argv
+from urllib.request import urlopen
+
+
+if __name__ == "__main__":
+    with urlopen(argv[1]) as response:
+        headers = response.headers
+    print(dict(headers)['X-Request-Id'])
