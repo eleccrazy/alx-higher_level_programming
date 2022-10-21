e6d22eeb848bf77736f166bf415e70775705f4ac
Gizachew Bayness <eleccrazy24@gmail.com>
2022-10-21 23:18:00 +0300

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This pyhton module contains a script that takes in a letter and
+sends a POST request to http://0.0.0.0:5000/search_user with the
+letter as a parameter."""
+from sys import argv
+from requests import post
+
+if __name__ == "__main__":
+    payload = {"q": "" if not argv[1] else argv[1]}
+    result = post("http://0.0.0.0:5000/search_user", data=payload)
+
+    try:
+        response = result.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
