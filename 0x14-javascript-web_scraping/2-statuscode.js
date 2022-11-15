3ad85ae898437183db7526c52b92be9eaa6710e0
Gizachew Bayness <eleccrazy24@gmail.com>
2022-11-15 14:09:43 +0300

    task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const args = process.argv;
+const request = require('request');
+request(args[2], function (error, response, body) {
+  if (error === null) console.log('code: ', response.statusCode);
+});
