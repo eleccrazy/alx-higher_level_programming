58e8c0dbfa97e8ea7f21fbc87a5014a675dce946
Gizachew Bayness <eleccrazy24@gmail.com>
2022-11-15 14:12:01 +0300

    make status code string

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,5 @@
 const args = process.argv;
 const request = require('request');
 request(args[2], function (error, response, body) {
-  if (error === null) console.log('code: ', response.statusCode);
+  if (error === null) console.log('code: ', response.statusCode.toString());
 });
